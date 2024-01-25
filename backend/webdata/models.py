9d514f2aeec825a11fb0ec9b77d38ee2a35f53f8
JeffTimoman <ppti.jeffersontimotius@gmail.com>
2024-01-25 22:08:02 +0700

    done

--- backend/webdata/models.py
+++ backend/webdata/models.py
@@ -13,6 +13,7 @@ from webdata import jwt
 
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     password = db.Column(db.String(100))
