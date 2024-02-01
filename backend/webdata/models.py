df4e290b5ec76433fd8daa8d3bc662fe9c7b3898
Nick Mathew <nickmathew331@gmail.com>
2024-02-01 14:58:19 +0700

    Edit DB Variables

--- backend/webdata/models.py
+++ backend/webdata/models.py
@@ -1,7 +1,7 @@
 from webdata import db, app
 from webdata import jwt
 
-#Table List:
+# Table List:
 # - User
 # - BahanMakanan
 # - ResepMakanan
@@ -17,64 +17,67 @@ class User(db.Model):
     password = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
     username = db.Column(db.String(100), unique=True)
-    tanggallahir = db.Column(db.DateTime)
-
-    @property
-    def formatted_tanggal_lahir(self):
-        return self.tanggallahir.strftime("%d%m%Y")
+    birth = db.Column(db.DateTime)
+    is_admin = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return f'<User: {self.name}>'
 
-class ResepFavorit(db.Model):
-    __tablename__ = 'resepfavorite'
-    id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
-    id_resepfavorit = db.Column(db.Integer, db.ForeignKey('resepmakanan.id_resep'), primary_key=True)
+
+class FavoriteRecipe(db.Model):
+    __tablename__ = 'favoriterecipe'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), primary_key=True)
 
 
-class BahanMakanan(db.Model):
-    __tablename__ = 'bahanmakanan'
-    id_bahan = db.Column(db.Integer, primary_key=True)
-    id_nutrisi = db.Column(db.Integer, db.ForeignKey('nutrisi.id_nutrisi'))
-    namabahan = db.Column(db.String(100))
-    deskripsibahan = db.Column(db.String(200))
+class Ingredient(db.Model):
+    __tablename__ = 'ingredients'
+    id = db.Column(db.Integer, primary_key=True)
+    nutrition_id = db.Column(db.Integer, db.ForeignKey('nutritions.id'))
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(200))
 
-class ResepMakanan(db.Model):
-    __tablename__ = 'resepmakanan'
-    id_resep = db.Column(db.Integer, primary_key=True)
-    namamakanan = db.Column(db.String(100), unique=True)
-    langkahpembuatan = db.Column(db.String(200))
-    jumlahporsi = db.Column(db.Integer)
-    waktumemasak = db.Column(db.Integer) #Integer karena waktu memasak dalam menit sehingga perlu memasukkan waktu memasak dalam menit
 
-class Nutrisi(db.Model):
-    __tablename__ = 'nutrisi'
-    id_nutrisi = db.Column(db.Integer, primary_key=True)
-    id_detailnutrisi = db.Column(db.Integer, db.ForeignKey('detailnutrisi.id_detailnutrisi'))
+class Recipe(db.Model):
+    __tablename__ = 'recipes'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    steps = db.Column(db.String(200))
+    portions = db.Column(db.Integer)
+    cooktime = db.Column(db.Integer)
+
+
+class Nutrition(db.Model):
+    __tablename__ = 'nutritions'
+    id = db.Column(db.Integer, primary_key=True)
+    nutritiondetail_id = db.Column(db.Integer, db.ForeignKey('nutritiondetails.id'))
     nama = db.Column(db.String(100), unique=True)
 
-class DetailNutrisi(db.Model):
-    __tablename__ = 'detailnutrisi'
-    id_detailnutrisi = db.Column(db.Integer, primary_key=True)
-    jumlahnutrisi = db.Column(db.Integer)
 
-class ResepMakananDetail(db.Model):
-    __tablename__ = 'resepmakanandetail'
-    id_resepdetail = db.Column(db.Integer, primary_key=True)
-    #Disini gw nambahin idResepDetail karena dari class diagram cuma ada idResep ama id, sedangkan perlu ada PK untuk tiap tabel
-    id_resep = db.Column(db.Integer, db.ForeignKey('resepmakanan.id_resep'))
-    id_bahan = db.Column(db.Integer, db.ForeignKey('bahanmakanan.id_bahan'))
-    jumlahbahan = db.Column(db.Float)
-    satuanjumlah = db.Column(db.String(50))
+class NutritionDetail(db.Model):
+    __tablename__ = 'nutritiondetails'
+    id = db.Column(db.Integer, primary_key=True)
+    nutritionamount = db.Column(db.Integer)
 
-class Artikel(db.Model):
-    __tablename__ = 'artikel'
-    idartikel = db.Column(db.Integer, primary_key = True)
-    judulartikel = db.Column(db.String(150), unique = True)
-    detailartikel = db.Column(db.String(1000))
-    namaaenulis = db.Column(db.String(100))
-    tanggalterbit = db.Column(db.DateTime)
+
+class RecipeDetail(db.Model):
+    __tablename__ = 'recipedetails'
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
+    ingredients_id = db.Column(db.Integer, db.ForeignKey('ingredients.id'))
+    ingredientamount = db.Column(db.Float)
+    unit = db.Column(db.String(50))
+
+
+class Article(db.Model):
+    __tablename__ = 'article'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), unique=True)
+    detail = db.Column(db.String(1000))
+    author = db.Column(db.String(100))
+    publishdate = db.Column(db.DateTime)
+    created_by = db.Column(db.Integer, db.ForeignKey("user.id"))
 
     @property
     def formatted_tanggal_terbit(self):
-        return self.tanggalterbit.strftime("%d%m%Y")
+        return self.publishdate.strftime("%d%m%Y")
