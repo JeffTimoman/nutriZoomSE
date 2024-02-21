e812eeb498f34f1541f5e6d969e9f9f6451401fa
JeffTimoman <ppti.jeffersontimotius@gmail.com>
2024-02-21 16:48:25 +0700

    Push

--- backend/webdata/main/routes.py
+++ backend/webdata/main/routes.py
@@ -8,3 +8,6 @@ main = Blueprint('main', __name__)
 def view_image(filename):
     # return url_for('static', filename=f'{app.config["FOLDER_UPLOAD_NAME"]}/{filename}')
     return redirect(url_for('static', filename=f'{app.config["FOLDER_UPLOAD_NAME"]}/{filename}'), code=301)
+@main.route('/')
+def index():
+    return redirect(url_for('admin.login'))
