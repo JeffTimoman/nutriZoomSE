8ba4fcef0a549901dd87e35974d765c73d142c08
JeffTimoman <ppti.jeffersontimotius@gmail.com>
2024-02-21 19:57:04 +0700

    fix final bug

--- backend/webdata/admin/routes.py
+++ backend/webdata/admin/routes.py
@@ -747,6 +747,8 @@ def edit_recipe(id):
     # print(ingredients)
     return render_template('admin/edit_recipe.html', recipe=recipe, ingredients=ingredients)
 
+
+@admin.route('/view_image')
 @admin.route('/view_image/<string:text>')
 def view_image(text='default.jpg'):
     return redirect(url_for('static', filename=f'{app.config["FOLDER_UPLOAD_NAME"]}/{text}'), code=301)
